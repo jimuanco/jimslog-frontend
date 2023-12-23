1291bec9bc0a660b6b955586f8e04c33fd8cc640
Jimoo An <jmdh1004@gmail.com>
2023-12-23 16:27:16 +0900

    :sparkles: 모바일 사이드바 추가

---
+++ src/components/ResponsiveConfig.js
@@ -0,0 +1,17 @@
+import { useMediaQuery } from "react-responsive"
+
+export const Mobile = ({children}) => {
+  const isMobile = useMediaQuery({
+    query: "(max-width:767px)"
+  });
+
+  return <>{isMobile && children}</>
+}
+
+export const PC = ({children}) => {
+  const isPc = useMediaQuery({
+    query : "(min-width:768px)"
+  });
+
+  return <>{isPc && children}</>
+}
