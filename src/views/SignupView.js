c087ca984bbf02fadc736f339aff7bf6eab1b8ca
Jimoo An <jmdh1004@gmail.com>
2024-01-18 21:59:10 +0900

    :lipstick: Login, signup 페이지 UI 수정

--- src/views/SignupView.js
+++ src/views/SignupView.js
@@ -17,7 +17,7 @@ const Signup = () => {
       password: password.current
     })
       .then(() => {
-        navigate("/", {replace: true});
+        navigate("/login", {replace: true});
       })
       .catch(() => {
 
@@ -31,22 +31,21 @@ const Signup = () => {
   }
 
   return (
-    <div>
-      <h1>회원가입</h1>
-      <div>
-        <label htmlFor="name">이름</label>
-        <input id="name" type="text" placeholder="이름을 입력하세요." required autoFocus onInput={(e) => {name.current = e.target.value}}/>
-
-        <label htmlFor="email">이메일</label>
-        <input id="email" type="email" placeholder="이메일을 입력하세요." required onInput={(e) => {email.current = e.target.value}}/>
-        
-        <label htmlFor="password">비밀번호</label>
-        <input id="password" type="password" placeholder="비밀번호를 입력하세요." minLength="8" maxLength="16" onInput={(e) => {password.current = e.target.value}}/>
-        
-        <button disabled={isSubmitting} onClick={handleSubmit}>
-          가입하기
-        </button>
+    <div className="signup-view">
+      <div className="signup-content">
+        <div className="name-box">
+          <input id="name" type="text" placeholder="이름" autoFocus onInput={(e) => {name.current = e.target.value}}/>
+        </div>
+        <div className="email-box">
+          <input id="email" type="email" placeholder="이메일" required onInput={(e) => {email.current = e.target.value}}/>
+        </div>
+        <div className="password-box">
+          <input id="password" type="password" placeholder="비밀번호" maxLength="16" onInput={(e) => {password.current = e.target.value}}/>
+        </div>
       </div>
+      <button disabled={isSubmitting} onClick={handleSubmit}>
+        가입하기
+      </button>
     </div>
   )
 }
